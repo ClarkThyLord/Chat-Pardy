f59047e621a42005bdac51d29173666352e7a9e0
isalu18 <luke_shakz@hotmail.com>
2018-10-26 09:45:16 -0700

    arreglado problema 1de2

--- src/renderer/components/hub/group-space.vue
+++ src/renderer/components/hub/group-space.vue
@@ -1,13 +1,20 @@
+
 <template>
   <div class="d-flex bd-highlight flex-wrap">
     <div style="max-width: 18rem;" class="m-2 flex-fill bd-highlight card text-white bg-primary">
-      <div class="card-header">Group 1</div>
+      <div class="card-header text-center">Group 1</div>
 
-      <div style="overflow-x: hidden; overflow-y: auto; border-width: 4px !important; width: 200px; min-width: 200px; !important; word-wrap: break-word; border-style: dashed !important;" class="m-1 flex-small-fill bd-highlight card-body border">
+      <div style="overflow-x: hidden; overflow-y: auto; border-width: 4px !important; width: 200px; min-width: 200px; !important; word-wrap: break-word; width: 97%; border-style: dashed !important;"class="m-1 flex-small-fill bd-highlight card-body border">
 
       </div>
 
     </div>
+
+
+
+
+
+
 
 
 
